aec945aa9efbc20ddabe7dd48df58dfea9689a34
takamoto0609 <takamoto0609@gamil.com>
2020-12-07 15:44:37 +0900

    サービス登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,29 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.valid?
+      Item.create(item_params)
+      redirect_to :root
+    else
+      @item = Item.new(item_params)
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:wash_power_id, :dry_power_id, :start_time_id, :end_time_id, :status_id, :point, :comment, :address, :user_id, :latitude, :longitude).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :wash_power
+  belongs_to :dry_power
+  belongs_to :start_time
+  belongs_to :end_time
+  belongs_to :status
+
+  validates :comment, :address, :point, presence: true
 end
